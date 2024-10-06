d97c28c08b6f5bf1d619ad13e6108e5a2b1ff633
Darwin Zamarca G <dzamarca@subsole.com>
2024-10-06 13:31:59 -0300

    add - carga inicial

---
+++ assets/js/array.js
@@ -0,0 +1,137 @@
+export const propiedades_alquiler = [
+    {
+        nombre: "Apartamento en alquiler 1",
+        src: "https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&w=600",
+        descripcion: "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+        ubicacion: 'Los Lagos, Chile',
+        habitaciones: 4,
+        banos: 3,
+        costo: 5000,
+        smoke: false,
+        pets: false,
+    },
+    {
+        nombre: "Apartamento en alquiler 2",
+        src: 'https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&w=600',
+        descripcion: "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+        ubicacion: 'La Araucanía, Chile',
+        habitaciones: 5,
+        banos: 2,
+        costo: 2500,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: "Apartamento en alquiler 3",
+        src: 'https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=600',
+        descripcion: "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+        ubicacion: 'Magallanes, Chile',
+        habitaciones: 6,
+        banos: 3,
+        costo: 3000,
+        smoke: false,
+        pets: true
+    },
+    {
+        nombre: "Apartamento en alquiler 4",
+        src: 'https://images.pexels.com/photos/164558/pexels-photo-164558.jpeg?auto=compress&cs=tinysrgb&w=600',
+        descripcion: "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+        ubicacion: 'Aysen, Chile',
+        habitaciones: 5,
+        banos: 4,
+        costo: 2200,
+        smoke: true,
+        pets: false
+    },
+    {
+        nombre: "Apartamento en alquiler 5",
+        src: 'https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&w=600',
+        descripcion: "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+        ubicacion: 'Los Rios, Chile',
+        habitaciones: 5,
+        banos: 3,
+        costo: 3200,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: "Apartamento en alquiler 6",
+        src: 'https://images.pexels.com/photos/1438832/pexels-photo-1438832.jpeg?auto=compress&cs=tinysrgb&w=600',
+        descripcion: "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+        ubicacion: 'Los Lagos, Chile',
+        habitaciones: 4,
+        banos: 4,
+        costo: 1500,
+        smoke: true,
+        pets: true
+    },
+];
+
+export const propiedades_venta =[
+    {
+        nombre: 'Casa en venta 1',
+        src: 'https://images.pexels.com/photos/280222/pexels-photo-280222.jpeg?auto=compress&cs=tinysrgb&w=600',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        ubicacion: 'Los Lagos, Chile',
+        habitaciones: 3,
+        banos: 2,
+        costo: 1000,
+        smoke: false,
+        pets: true
+    },
+    {
+        nombre: 'Casa en venta 2',
+        src: 'https://images.pexels.com/photos/221540/pexels-photo-221540.jpeg?auto=compress&cs=tinysrgb&w=600',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        ubicacion: 'Los Rios, Chile',
+        habitaciones: 4,
+        banos: 2,
+        costo: 3000,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Casa en venta 3',
+        src: 'https://images.pexels.com/photos/277667/pexels-photo-277667.jpeg?auto=compress&cs=tinysrgb&w=800',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        ubicacion: 'Los Rios, Chile',
+        habitaciones: 6,
+        banos: 4,
+        costo: 2000,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Casa en venta 4',
+        src: 'https://images.pexels.com/photos/209315/pexels-photo-209315.jpeg?auto=compress&cs=tinysrgb&w=800',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        ubicacion: 'La Araucanía, Chile',
+        habitaciones: 5,
+        banos: 4,
+        costo: 1400,
+        smoke: true,
+        pets: false
+    },
+    {
+        nombre: 'Casa en venta 5',
+        src: 'https://images.pexels.com/photos/53610/large-home-residential-house-architecture-53610.jpeg?auto=compress&cs=tinysrgb&w=800',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        ubicacion: 'Aysen, Chile',
+        habitaciones: 6,
+        banos: 4,
+        costo: 1300,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Casa en venta 6',
+        src: 'https://images.pexels.com/photos/259593/pexels-photo-259593.jpeg?auto=compress&cs=tinysrgb&w=800',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        ubicacion: 'Magallanes, Chile',
+        habitaciones: 3,
+        banos: 2,
+        costo: 1200,
+        smoke: false,
+        pets: true
+    }
+];
